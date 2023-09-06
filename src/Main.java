7f355ddc0ce444e1655a33985efda9f715ea6bf5
markup54 <marzenakupka@tlen.pl>
2023-09-06 15:22:13 +0200

    switch

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,21 @@ public class Main {
             wyrażenie warunkowe
             warunek ? jezeli prawda : jezeli fałsz
              */
+            roznica = roznica /10;
+            //dzielenie calkowite, jeżeli dzielimy liczby całkowite to wynik też jest calkowity
+            switch (roznica){
+                case 0:
+                    System.out.println("Było bardzo blisko");
+                    break;
+                case 1:
+                    System.out.println("Niezle");
+                    break;
+                case 2:
+                    System.out.println("trochę brakowało");
+                    break;
+                default:
+                    System.out.println("Może innym razem będzie lepiej");
+            }
         }
 
     }
