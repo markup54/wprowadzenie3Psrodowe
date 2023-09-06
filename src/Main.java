b6d0a0be0e8256329e9aff8f267c750791f5289f
markup54 <marzenakupka@tlen.pl>
2023-09-06 14:59:30 +0200

    wylosowanie liczby

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("Zostanie wylosowana jedna liczba z zakresu od 1 do 100");
+        int liczbaWylosowana = (int)(Math.random()*100+1);//zakres <0,1)
+        System.out.println(liczbaWylosowana);
+        /*
+        rzutowanie z double do int
+        rzutowanie zawezajace (int)
+
+        typy proste
+        boolean
+        char
+
+        byte
+        int
+        byte do int rzutowanie które nic nie traci
+        z int do byte zawezam
+
+         */
+
     }
 }
