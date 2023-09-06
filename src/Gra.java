51a335f4162c64d97a4fb943792a10c28f080363
markup54 <marzenakupka@tlen.pl>
2023-09-06 17:31:13 +0200

    gra w klasie

---
+++ src/Gra.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Gra {
+    private HashSet<Integer> wylosowane = new HashSet<>();
+    private ArrayList<Integer> liczbyWczytane = new ArrayList<>();
+    private LinkedList<Integer> trafione =new LinkedList<>();
+
+    private void wylosujLiczby(){
+        System.out.println("Zostanie wylosowanych 6 liczb z zakresu od 1 do 100");
+        while(wylosowane.size()<6){
+            wylosowane.add((int) (Math.random() * 100 + 1));
+        }
+    }
+    private void wczytajLiczby(){
+        //Wczytywać z klawiatury 6 liczb bez powtorzen
+        Scanner klawiatura = new Scanner(System.in);
+        for(int i=0;i<6;i++){
+            int liczba;
+            do {
+                System.out.println("Podaj liczbę");
+                liczba = klawiatura.nextInt();
+
+            }while (liczbyWczytane.contains(liczba));
+            liczbyWczytane.add(liczba);
+        }
+
+    }
+    private void sprawdzWyniki(){
+        for(Integer wylosowana:wylosowane ){
+            if(liczbyWczytane.contains(wylosowana)){
+                trafione.add(wylosowana);
+            }
+        }
+    }
+    public void zagraj(){
+        wylosujLiczby();
+        wczytajLiczby();
+        sprawdzWyniki();
+        System.out.println("Wczytane"+liczbyWczytane);
+        System.out.println("Wylosowane"+wylosowane);
+        System.out.println("Trfione"+trafione);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -5,52 +5,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static HashSet<Integer> wylosujLiczby(){
-        System.out.println("Zostanie wylosowanych 6 liczb z zakresu od 1 do 100");
-        HashSet<Integer> wylosowane = new HashSet<>();
-        while(wylosowane.size()<6){
 
-            wylosowane.add((int) (Math.random() * 100 + 1));
-        }
-        return wylosowane;
-    }
-    private static ArrayList<Integer> wczytajLiczby(){
-        //Wczytywać z klawiatury 6 liczb bez powtorzen
-        Scanner klawiatura = new Scanner(System.in);
-        ArrayList<Integer> liczbyWczytane = new ArrayList<>();
-        for(int i=0;i<6;i++){
-            int liczba;
-            do {
-                System.out.println("Podaj liczbę");
-                liczba = klawiatura.nextInt();
-
-            }while (liczbyWczytane.contains(liczba));
-            liczbyWczytane.add(liczba);
-        }
-        return liczbyWczytane;
-    }
-private static LinkedList<Integer> sprawdzWyniki(HashSet<Integer>wylosowane,
-                                                 ArrayList<Integer>liczbyWczytane){
-    LinkedList<Integer> trafione = new LinkedList<>();
-    //jeżeli jakaś liczba będzie i w wylosowane i w wpisane to dać do trafione
-    for(Integer wylosowana:wylosowane ){
-        if(liczbyWczytane.contains(wylosowana)){
-            trafione.add(wylosowana);
-        }
-    }
-    return trafione;
-}
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        HashSet<Integer> wylosowane = wylosujLiczby();
-
-        ArrayList<Integer> liczbyWczytane = wczytajLiczby();
-
-        LinkedList<Integer> trafione = sprawdzWyniki(wylosowane,liczbyWczytane);
-
-        System.out.println("Trafiono "+trafione);
-
-
+        Gra graPierwsza = new Gra();
+        graPierwsza.zagraj();
+        graPierwsza.zagraj();
+        //TODO:Losowanie tylko raz i wpisywanie od nowa
     }
 }
