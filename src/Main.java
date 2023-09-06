8fe480867bcf59c3d7792ccb369061040b50cb68
markup54 <marzenakupka@tlen.pl>
2023-09-06 16:11:48 +0200

    losowanie 6 liczb do zbioru - nie bedzie powtorzen

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Zostanie wylosowanych 6 liczb z zakresu od 1 do 100");
-        int wylosowane[] = new int[6];
-        //tablica w Javie przechowuje typy proste i złożone
-        //musi miec ustalony rozmiar
-        //rozmiaru nie można zmienić w dalszym programie
-        for(int i=0;i<6;i++){
-            wylosowane[i]=(int) (Math.random() * 10 + 1);
+        HashSet<Integer> wylosowane = new HashSet<>();
+        /*
+        kolekcje:
+        listy - ArrayList, LinkedList
+        elementy uporządkowane indeksowane mogą sie powtarzać
+        zbiory - HashSet
+        elementy są zazwyczaj nieuporządkowane, nieindeksowane, nie moga się potwrzać
+        mapy
+         */
+        while(wylosowane.size()<6){
+
+            wylosowane.add((int) (Math.random() * 10 + 1));
         }
         System.out.println(wylosowane);//adres obiektu
         for(int wylosowana:wylosowane){
